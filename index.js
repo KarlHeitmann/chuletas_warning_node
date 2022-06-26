475abc467daccd4438c5d42a2c407521d09b7e53
Karl Heitmann <khf1988@gmail.com>
2022-06-25 23:07:00 -0400

    ejecuta git diff y muestra las diferencias

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+const { exec } = require('child_process');
+
+//const gitDiffResponse = (err, stdout, stderr) => {
+function gitDiffResponse(err, stdout, stderr) {
+  if (err) {
+    console.error(`exec error: ${err}`);
+    return;
+  }
+  console.log(stdout);
+}
 
 function main() {
-  console.log("Hello world");
+  const cmd1 = 'git diff --cached --name-only --diff-filter=ACM'
+  exec(cmd1, gitDiffResponse);
 }
+
 
 main();
 
